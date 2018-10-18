e667389eece5f56939062a90fe2026e28a47664b
danielcoker <danielcoker28@gmail.com>
2018-10-19 00:18:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+frontend-nanodegree-arcade-game
+===============================
+
+Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+The aim of the game is to move the player to the water without colliding with any enemy - the bug.
+
+## How to Play
+- Choose the image you want yor player to be.
+- Use the up, down, left and right arrow to move the player.
+- Move the player to the water.
+
+## Dependencies
+- Modal - Modal in pure javascript [https://lowrey.me/modals-in-pure-es6-javascript/]
---
+++ js/app.js
@@ -0,0 +1,181 @@
+// function to get cookie
+// getCookie script fro w3 schools
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+// A Game object to define some general game defautls
+class Game {
+    constructor() {
+        this.playerImage = getCookie('playerImage');
+    }
+}
+
+// Instatiating the game object
+let game = new Game();
+
+// set cookie playerImage to default image
+document.cookie = "playerImage = images/char-boy.png";
+
+// Get playerImmages container
+const playerDivImages = document.querySelector('.playerImages');
+
+// On click, change player immage and reload page.
+playerDivImages.addEventListener('click', function(event) {
+    let element = event.target;
+    if (element.hasAttribute('src')) {
+        const playerImage = element.getAttribute('src');
+        document.cookie = 'playerImage = ' + playerImage + '';
+        location.reload();
+    }
+});
+
+// Set clicked class to currently active player Image
+const playerDivChildren = playerDivImages.children;
+for (playerDivChild of playerDivChildren) {
+    playerDivChild.classList.remove('clicked');
+    if (playerDivChild.getAttribute('src') === game.playerImage) {
+        playerDivChild.classList.add('clicked');
+    }
+}
+
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x += this.speed * dt;
+
+    // Re-position enemies to the beginning of the board when they go off the convas.
+    if (this.x > 550) {
+        this.x = -100;
+        this.speed = 100 + Math.floor(Math.random() * 512);
+    }
+
+    // Check for collision between plater and enemy
+    // alert the player has collided with an enemy
+    if (player.x < this.x + 60 &&
+        player.x + 3 > this.x &&
+        player.y < this.y + 25 &&
+        30 + player.y > this.y) {
+            alert('You lost! You collided with an enemy.');
+            player.x = 200;
+            player.y = 300;
+        }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+const Player = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = game.playerImage;
+};
+
+Player.prototype.update = function() {
+    // Prevent player from moving off the canvas
+    if (this.y > 380) {
+        this.y = 380;
+    }
+
+    if (this.x > 400) {
+        this.x = 400;
+    }
+
+    if (this.x < 0) {
+        this.x = 0;
+    }
+
+    // check if player has won the game by reaching the top of the canvas
+    // show a 'you won' modal
+    // reset player back to the start position
+    if (this.y < 0) {
+        modal.open();
+        this.x = 200;
+        this.y = 380;
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPress) {
+    if (keyPress === 'left') {
+        this.x -= this.speed + 50;
+    } else if (keyPress === 'right') {
+        this.x += this.speed + 50;
+    } else if (keyPress === 'up') {
+        this.y -= this.speed + 30;
+    } else if (keyPress === 'down') {
+        this.y += this.speed + 30;
+    }
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+const allEnemies = [];
+
+const enemyPosition = [60, 140, 220];
+const player = new Player(200, 380, 50);
+let enemy;
+
+enemyPosition.forEach(function(posY) {
+    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
+    allEnemies.push(enemy);
+});
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+// Don't allow the browser scroll up when the up and down key is pressed.
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode === 38 || e.keyCode === 40) { // 38 === up and 40 === down
+        e.preventDefault();
+    }
+});
